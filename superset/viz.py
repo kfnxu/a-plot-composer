fca629af6838371bd2a4e45bf8b918f291f1a880
kfnxu <kxu@uw.edu>
2017-07-25 21:03:45 +0000

    prepare NVD3MultiChartViz for sql return dataset to include charttype field to indicate the type of chart in nvd3 multichart, exampe 'type=scatter'

--- superset/viz.py
+++ superset/viz.py
@@ -1059,13 +1059,81 @@ class HistogramViz(BaseViz):
         chart_data = df[df.columns[0]].values.tolist()
         return chart_data
 
-class NVD3MultiChartViz(NVD3DualLineViz):
+#class NVD3MultiChartViz(NVD3DualLineViz):
+class NVD3MultiChartViz(NVD3Viz):
 
     """A multichart """
 
     viz_type = "multichart"
-    sort_series = True
+    sort_series = False 
     verbose_name = _("MultiChart")
+    is_timeseries = True
+
+    def query_obj(self):
+        d = super(NVD3MultiChartViz, self).query_obj()
+        m1 = self.form_data.get('metric')
+        m2 = self.form_data.get('metric_2')
+        d['metrics'] = [m1, m2]
+        if not m1:
+            raise Exception("Pick a metric for left axis!")
+        if not m2:
+            raise Exception("Pick a metric for right axis!")
+        if m1 == m2:
+            raise Exception("Please choose different metrics"
+                            " on left and right axis")
+        return d
+
+    def to_series(self, df, classed=''):
+        cols = []
+        for col in df.columns:
+            if col == '':
+                cols.append('N/A')
+            elif col is None:
+                cols.append('NULL')
+            else:
+                cols.append(col)
+        df.columns = cols
+        series = df.to_dict('series')
+        chart_data = []
+        metrics = [
+            self.form_data.get('metric'),
+            self.form_data.get('metric_2')
+        ]
+        for i, m in enumerate(metrics):
+            ys = series[m]
+            if df[m].dtype.kind not in "biufc":
+                continue
+            series_title = m
+            d = {
+                "key": series_title,
+                "classed": classed,
+                "values": [
+                    {'x': ds, 'y': ys[ds] if ds in ys else None}
+                    for ds in df.index
+                ],
+                "yAxis": i+1,
+                "type": "line"
+            }
+            chart_data.append(d)
+        return chart_data
+
+    def get_data(self, df):
+        fd = self.form_data
+        df = df.fillna(0)
+
+        if self.form_data.get("granularity") == "all":
+            raise Exception("Pick a time granularity for your time series")
+
+        metric = fd.get('metric')
+        metric_2 = fd.get('metric_2')
+        df = df.pivot_table(
+            index=DTTM_ALIAS,
+            values=[metric, metric_2])
+
+        chart_data = self.to_series(df)
+        return chart_data
+
+
 
 class DistributionBarViz(DistributionPieViz):
 
